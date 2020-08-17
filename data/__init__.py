b34602b63407b1f4c470ff77f645a7668177e111
Dong Gong <edgong01@gmail.com>
2020-08-17 10:33:15 +0930

    update data init

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import print_function, absolute_import
 from .blurry_image_dataset import *
+from .make_kernel import *
 # from data.blurry_image_dataset import BlurryImageDataset, ToTensor
